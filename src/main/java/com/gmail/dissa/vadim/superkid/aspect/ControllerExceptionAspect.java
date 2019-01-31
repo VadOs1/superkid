f6da5508b46167c8445b14b8977099102b310206
Vadim Dissa <vadim.dissa@gmail.com>
2019-01-30 19:05:25 -0500

    Clean logger in ControllerExceptionAspect

--- src/main/java/com/gmail/dissa/vadim/superkid/aspect/ControllerExceptionAspect.java
+++ src/main/java/com/gmail/dissa/vadim/superkid/aspect/ControllerExceptionAspect.java
@@ -8,16 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class ControllerExceptionAspect {
-
-    private final Logger logger;
-
-    public ControllerExceptionAspect() {
-        logger = Logger.getLogger(ControllerExceptionAspect.class);
-    }
-
-    public ControllerExceptionAspect(Logger logger) {
-        this.logger = logger;
-    }
+    private final Logger logger = Logger.getLogger(ControllerExceptionAspect.class);
 
     // Catching all exceptions from all methods in all controllers classes
     @AfterThrowing(pointcut = "execution(* com.gmail.dissa.vadim.superkid..controller..*(..))", throwing = "exception")
