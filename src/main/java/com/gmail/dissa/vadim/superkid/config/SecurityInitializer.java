6cdc439dd9fa716d3f633e91c4d417738d0d9d09
Vadim Dissa <vadim.dissa@gmail.com>
2019-01-31 19:39:01 -0500

    Clean SecurityInitializer class

--- src/main/java/com/gmail/dissa/vadim/superkid/config/SecurityInitializer.java
+++ src/main/java/com/gmail/dissa/vadim/superkid/config/SecurityInitializer.java
@@ -2,6 +2,5 @@ package com.gmail.dissa.vadim.superkid.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-// Initializes Spring Security
 public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 }
