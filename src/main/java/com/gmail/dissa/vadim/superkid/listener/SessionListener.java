226c039c59e5052d4815eab52ea6708631e8bb14
Vadim Dissa <vadim.dissa@gmail.com>
2019-02-04 19:35:33 -0500

    Clean SessionListener

--- src/main/java/com/gmail/dissa/vadim/superkid/listener/SessionListener.java
+++ src/main/java/com/gmail/dissa/vadim/superkid/listener/SessionListener.java
@@ -1,19 +1,16 @@
 package com.gmail.dissa.vadim.superkid.listener;
 
-
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-
 public class SessionListener implements HttpSessionListener {
-
-    // Setting manual session time
     @Override
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
         httpSessionEvent.getSession().setMaxInactiveInterval(90 * 24 * 60 * 60);
-
     }
-    // Add later if needed
+
     @Override
-    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {}
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+        // do nothing
+    }
 }
