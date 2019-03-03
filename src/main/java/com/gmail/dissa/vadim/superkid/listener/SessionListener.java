a6b182b4ebcbefaff8a0068f6823987c698ae0ab
Vadim Dissa <vadim.dissa@gmail.com>
2019-03-03 00:45:57 -0500

    Make session listener a bean

--- src/main/java/com/gmail/dissa/vadim/superkid/listener/SessionListener.java
+++ src/main/java/com/gmail/dissa/vadim/superkid/listener/SessionListener.java
@@ -1,12 +1,15 @@
 package com.gmail.dissa.vadim.superkid.listener;
 
+import org.springframework.stereotype.Component;
+
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
+@Component
 public class SessionListener implements HttpSessionListener {
     @Override
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
-        httpSessionEvent.getSession().setMaxInactiveInterval(90 * 24 * 60 * 60);
+        httpSessionEvent.getSession().setMaxInactiveInterval(60 * 60 * 365);
     }
 
     @Override
