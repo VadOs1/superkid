7863396471a8e8d4e3289de665796c1d65a351d1
Vadim Dissa <vadim.dissa@gmail.com>
2019-05-02 09:57:59 -0400

    Add health resource

---
+++ src/main/java/com/gmail/dissa/vadim/superkid/rest/HealthResource.java
@@ -0,0 +1,17 @@
+package com.gmail.dissa.vadim.superkid.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthResource {
+
+    @GetMapping
+    public ResponseEntity<?> healthCheck() {
+        return ResponseEntity.status(HttpStatus.OK).body("OK");
+    }
+}
