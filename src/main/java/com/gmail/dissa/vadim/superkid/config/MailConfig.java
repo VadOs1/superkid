fd58306cf61deb3535a90d0dbf6e35e3dae2a574
Vadim Dissa <vadim.dissa@gmail.com>
2019-06-01 10:24:08 -0400

    Move email credentials to env variables

--- src/main/java/com/gmail/dissa/vadim/superkid/config/MailConfig.java
+++ src/main/java/com/gmail/dissa/vadim/superkid/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.gmail.dissa.vadim.superkid.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.MailSender;
@@ -10,13 +11,19 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value(value = "${superkid.mail.username}")
+    private String username;
+
+    @Value(value = "${superkid.mail.password}")
+    private String password;
+
     @Bean
     public MailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("mail@gmail.com");
-        mailSender.setPassword("password");
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
         mailSender.setDefaultEncoding("UTF-8");
 
         Properties props = mailSender.getJavaMailProperties();
