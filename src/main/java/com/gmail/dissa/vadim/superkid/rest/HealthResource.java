d92b69926a0472083776194ad2e1a485818523ba
Vadim Dissa <vadim.dissa@gmail.com>
2019-06-14 17:04:07 -0400

    Change HealthResource

--- src/main/java/com/gmail/dissa/vadim/superkid/rest/HealthResource.java
+++ src/main/java/com/gmail/dissa/vadim/superkid/rest/HealthResource.java
@@ -1,5 +1,7 @@
 package com.gmail.dissa.vadim.superkid.rest;
 
+import com.gmail.dissa.vadim.superkid.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/health")
 public class HealthResource {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public HealthResource(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @GetMapping
     public ResponseEntity<?> healthCheck() {
+        try {
+            productRepository.findById(1L);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("ERROR");
+        }
         return ResponseEntity.status(HttpStatus.OK).body("OK");
     }
 }
