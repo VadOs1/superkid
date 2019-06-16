a2c93ac88f5afab28815b9cc920ca91125ffe0da
Vadim Dissa <vadim.dissa@gmail.com>
2019-06-16 17:37:56 -0400

    Replace http with https in SeoController

--- src/main/java/com/gmail/dissa/vadim/superkid/controller/SeoController.java
+++ src/main/java/com/gmail/dissa/vadim/superkid/controller/SeoController.java
@@ -16,7 +16,7 @@ public class SeoController {
         return "User-agent: *" +
                 "\n" + "Disallow: /admin/" +
                 "\n" + "Host: superkid.com.ua" +
-                "\n" + "Sitemap: http://superkid.com.ua/sitemap.xml";
+                "\n" + "Sitemap: https://superkid.com.ua/sitemap.xml";
     }
 
     // Producing sitemap.xml file for search engines
@@ -30,82 +30,82 @@ public class SeoController {
                 "      xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9\n" +
                 "            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/</loc>\n" +
+                "  <loc>https://superkid.com.ua/</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/cart</loc>\n" +
+                "  <loc>https://superkid.com.ua/cart</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/300101</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/300101</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/300102</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/300102</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/300103</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/300103</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/300104</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/300104</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/300105</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/300105</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/300106</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/300106</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/300107</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/300107</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/300108</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/300108</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/400101</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/400101</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/400102</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/400102</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/400103</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/400103</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/400104</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/400104</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/400105</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/400105</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/400106</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/400106</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/400107</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/400107</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/400108</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/400108</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/500101</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/500101</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/500102</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/500102</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/500103</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/500103</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/500104</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/500104</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/500105</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/500105</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/500106</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/500106</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/500107</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/500107</loc>\n" +
                 "</url>\n" +
                 "<url>\n" +
-                "  <loc>http://superkid.com.ua/products/500108</loc>\n" +
+                "  <loc>https://superkid.com.ua/products/500108</loc>\n" +
                 "</url>\n" +
                 "</urlset>";
     }
