d9b029c51b7cf5c20365d90944ccf2a928a13802
Vadim Dissa <vadim.dissa@gmail.com>
2019-06-22 21:11:25 -0400

    Add configuration properties

--- src/main/java/com/gmail/dissa/vadim/superkid/config/MailConfig.java
+++ src/main/java/com/gmail/dissa/vadim/superkid/config/MailConfig.java
@@ -1,32 +1,31 @@
 package com.gmail.dissa.vadim.superkid.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.gmail.dissa.vadim.superkid.properties.Properties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import java.util.Properties;
-
 @Configuration
 public class MailConfig {
+    private final Properties properties;
 
-    @Value(value = "${superkid.mail.username}")
-    private String username;
-
-    @Value(value = "${superkid.mail.password}")
-    private String password;
+    @Autowired
+    public MailConfig(Properties properties) {
+        this.properties = properties;
+    }
 
     @Bean
     public MailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
+        mailSender.setUsername(properties.getMail().getUsername());
+        mailSender.setPassword(properties.getMail().getPassword());
         mailSender.setDefaultEncoding("UTF-8");
 
-        Properties props = mailSender.getJavaMailProperties();
+        java.util.Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         return mailSender;
