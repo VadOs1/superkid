52f55cf26e7795f7efa688bb8c86dfeeae11709c
Vadim Dissa <vadim.dissa@gmail.com>
2019-06-23 15:32:16 -0400

    Add logging for health check

--- src/main/java/com/gmail/dissa/vadim/superkid/rest/HealthResource.java
+++ src/main/java/com/gmail/dissa/vadim/superkid/rest/HealthResource.java
@@ -1,6 +1,7 @@
 package com.gmail.dissa.vadim.superkid.rest;
 
 import com.gmail.dissa.vadim.superkid.repository.ProductRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Log4j2
 @RequestMapping("/health")
 public class HealthResource {
     private ProductRepository productRepository;
@@ -20,11 +22,13 @@ public class HealthResource {
 
     @GetMapping
     public ResponseEntity<?> healthCheck() {
+        log.info("Health check requested.");
         try {
             productRepository.findById(1L);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("ERROR");
         }
+        log.info("Health check completed.");
         return ResponseEntity.status(HttpStatus.OK).body("OK");
     }
 }
